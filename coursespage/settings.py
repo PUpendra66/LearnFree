034786b458548e2ec7b9f1c615e84b2d15acc3d6
upendra <pullariupendra66@gmail.com>
2024-12-27 14:56:43 +0530

    modified

--- coursespage/settings.py
+++ coursespage/settings.py
@@ -16,6 +16,7 @@ import pymysql
 pymysql.install_as_MySQLdb()
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
