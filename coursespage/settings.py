7e6ed517b508e7ce80de25c1672c049882ce179d
upendra <pullariupendra66@gmail.com>
2024-12-28 20:21:17 +0530

    'second'

--- coursespage/settings.py
+++ coursespage/settings.py
@@ -94,8 +94,7 @@ WSGI_APPLICATION = 'coursespage.wsgi.application'
 
 
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True,
     )
