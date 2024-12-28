e58c528a8825cfca5bd8c1eba320965c11989f00
upendra <pullariupendra66@gmail.com>
2024-12-28 20:31:11 +0530

    'first'

--- coursespage/settings.py
+++ coursespage/settings.py
@@ -94,11 +94,12 @@ WSGI_APPLICATION = 'coursespage.wsgi.application'
 
 
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
+    'default': dj_database_url.parse(
+        'postgresql://learnfreedb_user:Gw4k1hrDziyzUrsIbSz0LtJQ6M9quIOk@dpg-ctnvmrtds78s73c8dejg-a.oregon-postgres.render.com/learnfreedb'
     )
 }
+DATABASE_URL = os.getenv('DATABASE_URL')
+
 
 
 
